14c9a6bcaa71ea1f25618d8cbcf57090727aae49
carolineolagunju <olamidejr21@gmail.com>
2023-06-12 12:44:27 -0600

    refactored timer1

--- timer1.js
+++ timer1.js
@@ -1,11 +1,19 @@
-const times = process.argv.slice(2);
-
-for (let time of times) {
-  if (time < 0 || isNaN(time)) {
-    continue;
+const timer = function(timeArr) {
+//it sould not schedule an alarm if it is empty
+  if (timeArr.length === 0) {
+    return;
   }
-  time *= 1000;
-  setTimeout(() => {
-    process.stdout.write('\x07');
-  }, time);
-}
+
+  for (const time of timeArr) {
+    if (time < 1 || isNaN(time)) {
+      continue;
+    } else {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, time * 1000);
+    }
+  }
+};
+
+//Calling function with CLI arguments
+timer(process.argv.slice(2));
